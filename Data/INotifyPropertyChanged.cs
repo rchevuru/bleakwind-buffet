52d8be503ff8bcd6a6c00e5710a9e508c3df851e
rchevuru <rchevuru@CIS-RDS2>
2020-10-02 13:08:22 -0500

    Finish

---
+++ Data/INotifyPropertyChanged.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace BleakwindBuffet.Data
+{
+    interface INotifyPropertyChanged
+    {
+    }
+}
